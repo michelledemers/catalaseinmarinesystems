bce45aa490deea7110fc5311fe90b01fadc806b8
michelledemers <mademers2018@gmail.com>
2026-02-23 15:29:49 -0500

    Adding hmm files
    This upload includes the hmms, the code used to search the hmms, the raw hmm results (gene.out and gene.txt), and the filtered hmm results (gene_filtered.txt) for each catalase gene.

---
+++ HMMs/KatE-hmm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -n 10 #Request 4 tasks (cores)
+#SBATCH -N 1 #Request 1 node
+#SBATCH -t 2-2:00 #Request runtime of 30 minutes
+#SBATCH -C centos7 #Request only Centos7 nodes
+#SBATCH -p sched_mit_chisholm #Run on sched_engaging_default partition
+#SBATCH --mem-per-cpu=10000 #Request 4G of memory per CPU
+#SBATCH -o output_%j.txt #redirect output to output_JOBID.txt
+#SBATCH -e error_%j.txt #redirect errors to error_JOBID.txt
+#SBATCH --mail-type=BEGIN,END #Mail when job starts and ends
+#SBATCH --mail-user=demers@mit.edu #email recipient
+
+hmmsearch --tblout KatE.out --noali --cpu 10 KatE.hmm ../../DOSTraits/genes-redundant.faa
